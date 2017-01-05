3dd8e1a72532574051dccb6561b230cc98ec8dad
z.aliyev <z.aliyev@riberry.az>
2017-01-05 12:33:05 +0300

    Test pull before push1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello World from pro1!");
         System.out.println("Hello World from pro2!");
+        System.out.println("Test pull before push1");
     }
 }
