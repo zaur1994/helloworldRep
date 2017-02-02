557f73eb0f28568eb94ae84bbb96edd0f9781279
Zaur <z.aliyev@riberry.az>
2017-02-02 23:37:24 +0400

    Hello World from second project!

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World from pro1!");
-        System.out.println("Hello World from pro2!");
         System.out.println("Hello World from second project!");
     }
 }
