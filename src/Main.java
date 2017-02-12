1bd82dd54ad5dba3d308a6a16ecee0ebd135eec9
Zaur <z.aliyev@riberry.az>
2017-02-12 23:05:41 +0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World from pro1!");
-        System.out.println("Hello World from pro2!");
-        System.out.println("Test pull before push2!");
-        System.out.println("Test pull before push1");
+
     }
 }
