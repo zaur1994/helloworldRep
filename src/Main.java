8f0dab76215288722d314e6b5c81dd00939bf182
Zaur <z.aliyev@riberry.az>
2017-02-12 23:26:07 +0400

    master branch changed from second local repository

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("master branch");
+        System.out.print("master branch changed from second local repository");
     }
 }
